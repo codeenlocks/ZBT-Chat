de9ee040e4883016bc0ec0eae52770299ae8f2b0
codeenlocks <igouwelink@gmail.com>
2026-01-16 22:44:27 +0100

    feat: infrastructure de base terminée - système d'inscription fonctionnel et templates Bootstrap configurés

--- chat/models.py
+++ chat/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Partie de Zainab (Les Salons)
 class Room(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
@@ -10,15 +9,12 @@ class Room(models.Model):
     def __str__(self):
         return self.name
 
-# Partie de Hermès (Les Messages) + celle de Becker (Modération)
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="messages")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    
-    # Pour la modération de Becker
-    is_flagged = models.BooleanField(default=False) 
+    is_flagged = models.BooleanField(default=False) # Pour la modération de Becker
 
     class Meta:
         ordering = ['timestamp']
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignupForm
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('signup') # À modifier plus tard vers la liste des salons
+    else:
+        form = SignupForm()
+    return render(request, 'chat/signup.html', {'form': form})
